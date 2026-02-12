68d1aafa62d81011cf8b259031a96be1dc4e0f79
Mamg00472 <miguel.martinezgarcia@educa.madrid.org>
2026-02-12 09:42:59 +0100

    App cambios

--- src/main/java/org/palomafp/videojuegoopiniones/App.java
+++ src/main/java/org/palomafp/videojuegoopiniones/App.java
@@ -1,5 +1,9 @@
 package org.palomafp.videojuegoopiniones;
 
+import java.util.ArrayList;
+
+import org.palomafp.videojuegoopiniones.modelo.Videojuego;
+
 /**
  * Aplicación principal para demostrar el modelo de datos de videojuegos
  */
@@ -7,10 +11,32 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        int menu = 0;
         System.out.println("=== SISTEMA DE OPINIONES DE VIDEOJUEGOS ===");
-        
+
         // Crear instancia de DatosDao que gestiona todas las entidades
-     
+
+        
+
+        do {
+            System.out.println("------------Menu------------");
+            System.out.println("1. mostrar videojuegos");
+            System.out.println("2. mostrar plataformas");
+            System.out.println("3. mostrar reseñas");
+            System.out.println("4. mostrar Empresas");
+            System.out.println("----------------------------");
+            
+            switch (menu) {
+                case 1:
+                    
+                    break;
+            
+                case 2:
+                    break;
+            }
+        } while (menu != 5);
+
         System.out.println("\n=== FIN DE LA DEMOSTRACIÓN ===");
     }
 }
